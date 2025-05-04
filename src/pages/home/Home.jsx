7966dbe3fb9a413f1ea8b5dd8aa4990706702d74
gabriellyheber22 <gabriellyheber@gmail.com>
2025-05-04 16:54:58 -0400

    Crianção da seção "Nosso Cmei" Criação da área de cards.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,8 +14,9 @@ function Home() {
        <h2>Bem-vindo ao nosso CMEI</h2>
        <p className="homeParagrafo">Aqui educação e carinho andam juntos para um crescimento feliz, propocionando ao seu filho um ambiente seguro e acolhedor! Onde criamos memórias felizes enquanto desenvolvemos um grande futuro.</p>
        <div className="cards"><Cards/></div>
-      
       </section>
+
+      
     </article>
   );
 }
