b935e3ce168ca9897c95238eb1d03e75e3a9bd99
gabriellyheber22 <gabriellyheber@gmail.com>
2025-05-05 21:42:55 -0400

    Criação da seção sobre nós

--- src/componentes/bordaRasgada/BordaRasgadaInferior.jsx
+++ src/componentes/bordaRasgada/BordaRasgadaInferior.jsx
@@ -4,23 +4,29 @@ import BordaRasgada from "../../assets/Imagens/designRasgadoInferior.svg";
 import BordaRasgadaPequena from "../../assets/Imagens/designRagasdoPequenoInferior.svg";
 import BordaRasgadaMedio from "../../assets/Imagens/designRagasdoMedioInferior.svg";
 import "./borda.css";
-function BordaRasgadaInferior() {
+function BordaRasgadaInferior({ className = "" }) {
+  
+
     return (
-     <><Imagem
-     src={BordaRasgada}
-     alt="Borda Rasgada"
-     className="bordaInferiorGrande"
-   />
-   <Imagem
-     src={BordaRasgadaPequena}
-     alt="Borda Rasgada"
-     className="bordaInferiorPequena"
-   />
-   <Imagem
-     src={BordaRasgadaMedio}
-     alt="Borda Rasgada"
-     className="bordaInferiorMedio"
-   /></>
+     <>
+     <div className={className}>
+       <Imagem
+       src={BordaRasgada}
+       alt="Borda Rasgada"
+       className={`bordaInferiorGrande  ${className}`}
+          />
+          <Imagem
+       src={BordaRasgadaPequena}
+       alt="Borda Rasgada"
+       className={`bordaInferiorPequena  ${className}`}
+       />
+       
+          <Imagem
+       src={BordaRasgadaMedio}
+       alt="Borda Rasgada"
+       className={`bordaInferiorMedio  ${className}`}
+          />
+     </div></>
     );
   };
   
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import Imagem from "react-bootstrap/Image";
 import Carrossel from "../../componentes/carrossel/Carrossel";
-import Cards from "../../componentes/cards/Cards"
+import Cards from "../../componentes/cards/Cards";
+import meninoSorrindo from "../../assets/Imagens/meninoSorrindo.svg";
+import BordaInferior from "../../componentes/bordaRasgada/BordaRasgadaInferior";
+import BordaSuperior from "../../componentes/bordaRasgada/BordaRasgadaSuperior";
 import "./Home.css";
 
 function Home() {
@@ -9,14 +13,54 @@ function Home() {
       <section className="carrossel ">
         <Carrossel />
       </section>
-     
-      <section className="nossoCMEI">
-       <h2>Bem-vindo ao nosso CMEI</h2>
-       <p className="homeParagrafo">Aqui educação e carinho andam juntos para um crescimento feliz, propocionando ao seu filho um ambiente seguro e acolhedor! Onde criamos memórias felizes enquanto desenvolvemos um grande futuro.</p>
-       <div className="cards"><Cards/></div>
-      </section>
 
-      
+      <section className="nossoCMEI">
+        <h2>Bem-vindo ao nosso CMEI</h2>
+        <p className="homeParagrafo">
+          Aqui educação e carinho andam juntos para um crescimento feliz,
+          propocionando ao seu filho um ambiente seguro e acolhedor! Onde
+          criamos memórias felizes enquanto desenvolvemos um grande futuro.
+        </p>
+        <div className="cards">
+          <Cards />
+        </div>
+      </section>
+      <BordaSuperior className="bordaSuperior" />
+      <section className="sobreNos">
+        
+        <div className="sobreNos_info">
+          <div className="sobreNos_texto">
+            <h2>Sobre o CMEI Carlos Alberto Cruz</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+              quia, consectetur, alias fuga architecto optio, sit error eos
+              deserunt illo voluptate rem qui recusandae omnis rerum nesciunt
+              enim cumque aspernatur. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Repudiandae recusandae quasi quisquam. Deleniti,
+              dolor, aut corporis ab voluptates deserunt, laboriosam accusantium
+              amet facilis blanditiis fugit ut odio nostrum? Neque, odio?
+            </p>
+          </div>
+          <Imagem
+            className="sobreNos_imagem"
+            src={meninoSorrindo}
+            alt="Menino sorrindo mostrando as mãos"
+          />
+        </div>
+        
+      </section>
+      <BordaInferior className="bordaInferior" />
+      <section className="nossasSalas">
+        <div className="nossaSala_texto">
+          <h2>Nossas Salas de Aula</h2>
+          <p>
+            Nossas turmas são organizadas em três períodos: Integral, Parcial
+            Matutino e Parcial Vespertino, garantindo flexibilidade para atender
+            às necessidades das crianças e de suas famílias.{" "}
+          </p>
+        </div>
+        <div className="nossasSalas_cards"></div>
+      </section>
     </article>
   );
 }
